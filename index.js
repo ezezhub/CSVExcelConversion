b54acbd5056793851e4b6186fe9d581d8f7c24bc
eejet <eejet@mgb.com.sg>
2021-04-30 15:38:46 +0800

    Update the write function to Google Sheets.

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ const keys = require(googleConfig.credentials_path)
 
 var transactionList = [];
 
-// Load gclient from local keys file.
-const gclient = new google.auth.JWT(keys.client_email, null, keys.private_key, ['https://www.googleapis.com/auth/spreadsheets']);
-gclient.authorize(function (error, tokens) {
-    if (error) { console.log(error); return; }
-    else { console.log('Connected to Google Sheet Service...'); }
-});
+
 
 // Read the folder first, After completed ETL then write to Google Sheet.
 fs.readdir(folderDir, (err, files) => {
@@ -24,7 +19,7 @@ fs.readdir(folderDir, (err, files) => {
                 const temp = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], { range: 9 })
                 temp.forEach(function (transaction) {
                     if (transaction["Transaction Date"] != undefined) {
-                        transactionList.push(convertUOBTransaction(transaction))
+                        transactionList.push(convertUOBTransaction(transaction, "UOB PRIV"))
                     }
                 });
             } else {
@@ -32,9 +27,12 @@ fs.readdir(folderDir, (err, files) => {
                     var transaction = line.trim();
                     if (transaction.length > 0) {
                         if (file.includes("SCB")) {
-                            transactionList.push(convertSCBTransaction(transaction))
+                            transactionList.push(convertSCBTransaction(transaction, "SCB MANHATTEN"))
                         } else {
-                            transactionList.push(convertCitiTransaction(transaction))
+                            if (file.includes("508"))
+                                transactionList.push(convertCitiTransaction(transaction, "CITIBANK MILES"))
+                            else
+                                transactionList.push(convertCitiTransaction(transaction, "CITIBANK CASHBACK"))
                         }
                     };
                 })
@@ -47,34 +45,36 @@ fs.readdir(folderDir, (err, files) => {
             console.log("This is the UOB Bank: " + file)
         }
     })
-
+    var ModifiedTransactionList = [];
     transactionList.forEach(function (entry) {
-        console.log(entry);
+        temp = [entry["Card"], entry["Month"], entry["Amount"], entry["Remark"]];
+        ModifiedTransactionList.push(temp);
     });
 
-    const updateoptions = {
-        spreadsheetId: googleConfig.spreadsheetId,
-        range: googleConfig.spreadsheetId + "!A1",
-        valueInputOption: "USER_ENTERED",
-        resource: { values: transactionList }
-    };
-
-    let resp = google.spreadsheets.values.update(updateoptions);
-    console.log(resp);
+    // Load gclient from local keys file.
+    const gclient = new google.auth.JWT(keys.client_email, null, keys.private_key, ['https://www.googleapis.com/auth/spreadsheets']);
+    gclient.authorize(function (error, tokens) {
+        if (error) { console.log(error); return; }
+        else {
+            console.log('Connected to Google Sheet Service...');
+            updateGoogleSheet(gclient, ModifiedTransactionList);
+        }
+    });
 
 })
 
-function convertCitiTransaction(transaction) {
+function convertCitiTransaction(transaction, card) {
     var temp = transaction.split(",")
     var obj = {};
     var date = temp[0].replace(/^["'](.+(?=["']$))["']$/, '$1');
     obj["Month"] = parseInt(date.split("/")[1])
     obj["Amount"] = parseFloat(temp[2].replace(/^["'](.+(?=["']$))["']$/, '$1')) * -1;
     obj["Remark"] = temp[1].replace(/^["'](.+(?=["']$))["']$/, '$1');
+    obj["Card"] = card;
     return obj
 }
 
-function convertSCBTransaction(transaction) {
+function convertSCBTransaction(transaction, card) {
     var temp = transaction.split(",")
     var obj = {};
     var date = temp[0].replace(/^["'](.+(?=["']$))["']$/, '$1');
@@ -83,18 +83,34 @@ function convertSCBTransaction(transaction) {
     obj["Month"] = parseInt(date.split("/")[1])
     obj["Amount"] = parseFloat(amount.split(" ")[1]);
     obj["Remark"] = temp[1].replace(/^["'](.+(?=["']$))["']$/, '$1');
+    obj["Card"] = card;
     return obj
 }
 
-function convertUOBTransaction(transaction) {
+function convertUOBTransaction(transaction, card) {
     var obj = {};
     var date = transaction["Transaction Date"].split(" ")[1];
     obj["Month"] = getMonthFromString(date)
     obj["Amount"] = parseFloat(transaction["Transaction Amount(Local)"]);
     obj["Remark"] = transaction["Description"];
+    obj["Card"] = card;
     return obj
 }
 
 function getMonthFromString(mon) {
     return new Date(Date.parse(mon + " 1, 2012")).getMonth() + 1
+}
+
+async function updateGoogleSheet(client, transactionList) {
+    const gSheet = google.sheets({ version: 'v4', auth: client });
+
+    const updateoptions = {
+        spreadsheetId: googleConfig.spreadsheetId,
+        range: googleConfig.rangeId + "!A1:D1",
+        valueInputOption: "RAW",
+        resource: { values: transactionList }
+    };
+
+    let res = await gSheet.spreadsheets.values.append(updateoptions);
+    console.log("Update Completed...")
 }

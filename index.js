fae9ab9dc36c116e376279e3d458b2f1dac7bc0f
ezez <ezez85@gmail.com>
2021-05-01 14:39:19 +0800

    Added Natural Language Processing function

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 const config = require('config');
 const fs = require('fs');
 const XLSX = require('xlsx');
+const natural = require('natural');
+
 const googleConfig = config.get('configfile.googleapi-config');
-const folderDir = config.get('configfile.directory.path');
+const folderDir = config.get('configfile.directory-config');
+
 const { google } = require('googleapis');
 const keys = require(googleConfig.credentials_path)
+const sheetconfig = require(googleConfig.spreadsheet)
 
 var transactionList = [];
 
-
-
 // Read the folder first, After completed ETL then write to Google Sheet.
-fs.readdir(folderDir, (err, files) => {
+fs.readdir(folderDir.path, (err, files) => {
+
+
+    var classifier = classiferTraining(folderDir.model)
+
     files.forEach(file => {
         if (file.startsWith("CC")) {
             if (file.includes("TXN_History")) {
-                var workbook = XLSX.readFile(folderDir + "/" + file);
+                var workbook = XLSX.readFile(folderDir.path + "/" + file);
                 const temp = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], { range: 9 })
                 temp.forEach(function (transaction) {
                     if (transaction["Transaction Date"] != undefined) {
-                        transactionList.push(convertUOBTransaction(transaction, "UOB PRIV"))
+                        transactionList.push(convertUOBTransaction(transaction, "UOB PRIV", classifier))
                     }
                 });
             } else {
-                fs.readFileSync(folderDir + "/" + file, 'utf-8').split(/\r?\n/).forEach(function (line) {
+                fs.readFileSync(folderDir.path + "/" + file, 'utf-8').split(/\r?\n/).forEach(function (line) {
                     var transaction = line.trim();
                     if (transaction.length > 0) {
                         if (file.includes("SCB")) {
-                            transactionList.push(convertSCBTransaction(transaction, "SCB MANHATTEN"))
+                            transactionList.push(convertSCBTransaction(transaction, "SCB MANHATTEN", classifier))
                         } else {
                             if (file.includes("508"))
-                                transactionList.push(convertCitiTransaction(transaction, "CITIBANK MILES"))
+                                transactionList.push(convertCitiTransaction(transaction, "CITIBANK MILES", classifier))
                             else
-                                transactionList.push(convertCitiTransaction(transaction, "CITIBANK CASHBACK"))
+                                transactionList.push(convertCitiTransaction(transaction, "CITIBANK CASHBACK", classifier))
                         }
                     };
                 })
@@ -47,7 +53,7 @@ fs.readdir(folderDir, (err, files) => {
     })
     var ModifiedTransactionList = [];
     transactionList.forEach(function (entry) {
-        temp = [entry["Card"], entry["Month"], entry["Amount"], entry["Remark"]];
+        temp = [entry["Card"], entry["Month"], entry["Amount"], entry["Remark"], entry["TYPE"].replace(/^["'](.+(?=["']$))["']$/, '$1')];
         ModifiedTransactionList.push(temp);
     });
 
@@ -63,37 +69,54 @@ fs.readdir(folderDir, (err, files) => {
 
 })
 
-function convertCitiTransaction(transaction, card) {
+
+
+// Train the NLP using pre-defined Model.
+function classiferTraining(path) {
+    var classifier = new natural.BayesClassifier();
+    fs.readFileSync(path, 'utf-8').split(/\r?\n/).forEach(function (line) {
+        var training = line.trim().split(",");
+        classifier.addDocument(training[0], training[1]);
+    })
+    classifier.train();
+    return classifier;
+}
+
+function convertCitiTransaction(transaction, card, classifier) {
     var temp = transaction.split(",")
     var obj = {};
     var date = temp[0].replace(/^["'](.+(?=["']$))["']$/, '$1');
+    var remark = temp[1].replace(/^["'](.+(?=["']$))["']$/, '$1');
     obj["Month"] = parseInt(date.split("/")[1])
     obj["Amount"] = parseFloat(temp[2].replace(/^["'](.+(?=["']$))["']$/, '$1')) * -1;
-    obj["Remark"] = temp[1].replace(/^["'](.+(?=["']$))["']$/, '$1');
+    obj["Remark"] = remark
     obj["Card"] = card;
+    obj["TYPE"] = classifier.classify(remark);
     return obj
 }
 
-function convertSCBTransaction(transaction, card) {
+function convertSCBTransaction(transaction, card, classifier) {
     var temp = transaction.split(",")
     var obj = {};
     var date = temp[0].replace(/^["'](.+(?=["']$))["']$/, '$1');
     var amount = temp[3].replace(/^["'](.+(?=["']$))["']$/, '$1');
-
+    var remark = temp[1].replace(/^["'](.+(?=["']$))["']$/, '$1');
     obj["Month"] = parseInt(date.split("/")[1])
     obj["Amount"] = parseFloat(amount.split(" ")[1]);
-    obj["Remark"] = temp[1].replace(/^["'](.+(?=["']$))["']$/, '$1');
+    obj["Remark"] = remark
     obj["Card"] = card;
+    obj["TYPE"] = classifier.classify(remark);
     return obj
 }
 
-function convertUOBTransaction(transaction, card) {
+function convertUOBTransaction(transaction, card, classifier) {
     var obj = {};
     var date = transaction["Transaction Date"].split(" ")[1];
     obj["Month"] = getMonthFromString(date)
     obj["Amount"] = parseFloat(transaction["Transaction Amount(Local)"]);
     obj["Remark"] = transaction["Description"];
     obj["Card"] = card;
+    obj["TYPE"] = classifier.classify(transaction["Description"]);
     return obj
 }
 
@@ -105,8 +128,8 @@ async function updateGoogleSheet(client, transactionList) {
     const gSheet = google.sheets({ version: 'v4', auth: client });
 
     const updateoptions = {
-        spreadsheetId: googleConfig.spreadsheetId,
-        range: googleConfig.rangeId + "!A1:D1",
+        spreadsheetId: sheetconfig.spreadsheetId,
+        range: sheetconfig.rangeId + "!A1:D1",
         valueInputOption: "RAW",
         resource: { values: transactionList }
     };
